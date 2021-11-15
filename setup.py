0c0dc3f3c989f9322dd308f82ab940c3a438fc3e
Vitaly Chekryzhev <13hakta@gmail.com>
2021-11-15 08:46:15 +0000

    Add setup

---
+++ setup.py
@@ -0,0 +1,57 @@
+import os
+import re
+from codecs import open
+
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+long_description_content_type = "text/markdown"
+
+VERSION = "1.0.0"
+
+setup(
+    name="ipd",
+    version=VERSION,
+    description="An IPD - Image Push and Deploy",
+    long_description=long_description,
+    long_description_content_type=long_description_content_type,
+    url="https://bnipi-git-1.rosneft.ru/mobile/ipd",
+    author="Vitaly Chekryzhev",
+    author_email="VA_Chekryzhev@bnipi.rosneft.ru",
+    license="Proprietary",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+        "Topic :: System :: Networking :: Monitoring",
+        "Typing :: Typed",
+    ],
+    project_urls={
+        "Source": "https://bnipi-git-1.rosneft.ru/mobile/ipd/",
+    },
+    packages=find_packages(
+        include=[
+            "ipd",
+            "ipd.*",
+        ]
+    ),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "ipd = ipd.ipd:main",
+        ]
+    },
+    python_requires=">=3.6",
+    install_requires=[
+        "flask",
+        "waitress",
+    ],
+    extras_require={
+        "dev": [],
+    },
+)
